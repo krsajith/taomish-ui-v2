3185c17d665a432355453e254a434a520860d9cc
Sajith <krsajith@gmail.com>
2022-11-30 07:22:42 +0530

    grid

--- apps/ctrm/src/app/app.module.ts
+++ apps/ctrm/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { TuiGridComponent } from './tui-grid/tui-grid.component';
+
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, TuiGridComponent],
+  declarations: [AppComponent, NxWelcomeComponent, ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- libs/common-ui/src/lib/grid/grid.component.ts
+++ libs/common-ui/src/lib/grid/grid.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { GridCol } from './grid-model';
 
 @Component({
   selector: 'tui-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css'],
 })
-export class GridComponent {}
+export class GridComponent {
+  @Input() data!:unknown[];
+  @Input() cols!:GridCol[];
+}
