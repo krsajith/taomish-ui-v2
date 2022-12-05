45e98f29d48e86a67edc3dbdc2ac0f9f5021d163
Balmukund Pandey <balmukund.pandey@taomish.com>
2022-12-05 14:35:52 +0530

    change in json and add

--- libs/common-ui/src/lib/components/dynamic-form/dynamic-form.component.ts
+++ libs/common-ui/src/lib/components/dynamic-form/dynamic-form.component.ts
@@ -7,7 +7,7 @@ import { Store } from '../../store/store';
 import { Observable } from 'rxjs';
 import { BaseControlComponent } from '../../base-control/base-control.component';
 import { DynamicFormService } from '../../dynamic-form.service';
-import { Field, Page } from '../../page';
+import { Field, FieldStructure, Page } from '../../page';
 
 @Component({
   selector: 'tui-form',
@@ -40,23 +40,35 @@ export class DynamicFormComponent extends BaseControlComponent implements OnInit
   values: { [k: string]: Observable<any> } = {};
   parentValueChanges: { [k: string]: Observable<string> } = {};
 
+  fieldStructure:FieldStructure[]=[]
+
   constructor(private storeService: StoreService,private dynamicFormService:DynamicFormService) {
     super();
   }
 
   ngOnInit(): void {
     if(this.page) {
-      this.fields = this.dynamicFormService.getFields(this.page);
+      this.fieldStructure = this.dynamicFormService.getFields(this.page);
       if(!this.gridCols){
         this.gridCols=this.page.gridCols
       }
     }
 
-    this.formGroup = this.dynamicFormService.buildFormGroup(this.fields);
-    this.formGroup.valueChanges.subscribe(value=> this.onChange(value));
+    this.formGroup=new FormGroup({})
+
+    if(this.fields){
+      this.formGroup = this.dynamicFormService.buildFormGroup(this.fields);
+    }else{
+      this.fieldStructure.forEach(e=>{
+        this.formGroup.addControl(e.stepName,this.dynamicFormService.buildFormGroup(e.fields))
+      })
+    }
+
+    // this.formGroup = this.dynamicFormService.buildFormGroup(this.fields);
+    // this.formGroup.valueChanges.subscribe(value=> this.onChange(value));
     this.ready=true;
     this.formGroupRef.emit(this.formGroup)
-    console.log(this.fields);
+    console.log(this.formGroup);
   }
 
 
@@ -64,11 +76,15 @@ export class DynamicFormComponent extends BaseControlComponent implements OnInit
     return this.formGroup.invalid?{formGroup:'Invalid'} : null;
   }
 
-  getFormGroup():FormGroup{
-    return this.formGroup;
-  }
+  // getFormGroup():FormGroup{
+  //   return this.formGroup;
+  // }
 
   writeValue(obj: any): void {
     // throw new Error('Method not implemented.');
+  }
+
+  getFormGroup(fieldName:any):FormGroup{
+    return this.formGroup.get(fieldName) as FormGroup
   }
 }
